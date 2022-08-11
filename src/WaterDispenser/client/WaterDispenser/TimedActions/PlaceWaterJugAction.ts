0231ccb56b0f6590fa8c5b627164f4eab58a5527
Konijima <mathieugermain1991@gmail.com>
2022-08-11 16:18:06 -0400

    Completed timed actions

--- src/WaterDispenser/client/WaterDispenser/TimedActions/PlaceWaterJugAction.ts
+++ src/WaterDispenser/client/WaterDispenser/TimedActions/PlaceWaterJugAction.ts
@@ -1,37 +1,59 @@
-import { ISBaseTimedAction, ISLogSystem, IsoPlayer, ISTimedActionQueue } from "PipeWrench"
+import { DrainableComboItem, InventoryItem, ISBaseTimedAction, ISLogSystem, IsoPlayer, ISTimedActionQueue, Metabolics, sendClientCommand, _instanceof_ } from "PipeWrench"
+import { WaterDispenser } from "../../../shared/WaterDispenser/WaterDispenser"
 
-export function PlaceWaterJugAction(character: IsoPlayer) {
+export function PlaceWaterJugAction(character: IsoPlayer, waterDispenser: WaterDispenser, waterJugItem: InventoryItem) {
     const action = new ISBaseTimedAction(character)
     action.Type = "PlaceWaterJugAction"
-    action.maxTime = 120
     action.stopOnAim = true
     action.stopOnRun = true
     action.stopOnWalk = true
 
+    const usedDelta = ((_instanceof_(waterJugItem, "DrainableComboItem")) ? (waterJugItem as DrainableComboItem).getUsedDelta() : 0)
+
+    action.maxTime = 20 + 280 * usedDelta
+
+    const inventory = character.getInventory()
+
     // isValid
     action.isValid = () => {
-        return true
+        return waterDispenser.IsoObject && waterDispenser.Type == "None" && inventory.contains(waterJugItem)
     }
 
     // start
     action.start = () => {
-
+        waterJugItem.setJobType("Placing")
+        waterJugItem.setJobDelta(0)
+        action.setActionAnim("Loot")
     }
 
     // update
     action.update = () => {
-
+        waterJugItem.setJobDelta(action.getJobDelta())
+        character.faceThisObject(waterDispenser.IsoObject)
+        character.setMetabolicTarget(Metabolics.LightDomestic)
     }
 
     // stop
     action.stop = () => {
-        print(`${action.Type} stopped`)
+        waterJugItem.setJobDelta(0)
+
         ISTimedActionQueue.getTimedActionQueue(character).resetQueue()
     }
 
     // perform
     action.perform = () => {
-        print(`${action.Type} performed`)
+        waterJugItem.setJobDelta(0)
+
+        const args = {
+            x: waterDispenser.IsoObject.getX(),
+            y: waterDispenser.IsoObject.getY(),
+            z: waterDispenser.IsoObject.getZ(),
+            amount: 250 * usedDelta,
+            tainted: waterJugItem.isTaintedWater()
+        }
+        sendClientCommand(character, 'WaterDispenser', 'PlaceBottle', args)
+
+        inventory.Remove(waterJugItem)
 
         ISTimedActionQueue.getTimedActionQueue(character).onCompleted(action)
         ISLogSystem.logAction(action)
--- src/WaterDispenser/client/WaterDispenser/TimedActions/TakeWaterJugAction.ts
+++ src/WaterDispenser/client/WaterDispenser/TimedActions/TakeWaterJugAction.ts
@@ -1,37 +1,54 @@
-import { ISBaseTimedAction, ISLogSystem, IsoPlayer, ISTimedActionQueue } from "PipeWrench"
+import { DrainableComboItem, InventoryItem, ISBaseTimedAction, ISLogSystem, IsoPlayer, ISTimedActionQueue, Metabolics, sendClientCommand, _instanceof_ } from "PipeWrench"
+import { WaterDispenser } from "../../../shared/WaterDispenser/WaterDispenser"
 
-export function TakeWaterJugAction(character: IsoPlayer) {
+export function TakeWaterJugAction(character: IsoPlayer, waterDispenser: WaterDispenser) {
     const action = new ISBaseTimedAction(character)
     action.Type = "TakeWaterJugAction"
-    action.maxTime = 120
     action.stopOnAim = true
     action.stopOnRun = true
     action.stopOnWalk = true
+    action.maxTime = 20 + 280 * waterDispenser.getWaterAmount() / 250
+
+    const inventory = character.getInventory()
 
     // isValid
     action.isValid = () => {
-        return true
+        return waterDispenser.IsoObject && waterDispenser.Type != "None"
     }
 
     // start
     action.start = () => {
-
+        action.setActionAnim("Loot")
     }
 
     // update
     action.update = () => {
-
+        character.faceThisObject(waterDispenser.IsoObject)
+        character.setMetabolicTarget(Metabolics.LightDomestic)
     }
 
     // stop
     action.stop = () => {
-        print(`${action.Type} stopped`)
         ISTimedActionQueue.getTimedActionQueue(character).resetQueue()
     }
 
     // perform
     action.perform = () => {
-        print(`${action.Type} performed`)
+
+        const itemType = (waterDispenser.getWaterAmount() > 0) ? "WaterDispenser.WaterJugWaterFull" : "WaterDispenser.WaterJugEmpty"
+        const waterJugItem = inventory.AddItem(itemType) as InventoryItem
+        if (_instanceof_(waterJugItem, "DrainableComboItem")) {
+            const drainable = waterJugItem as DrainableComboItem
+            drainable.setUsedDelta(waterDispenser.getWaterAmount() / 250)
+            drainable.setTaintedWater(waterDispenser.getTainted())
+        }
+
+        const args = {
+            x: waterDispenser.IsoObject.getX(),
+            y: waterDispenser.IsoObject.getY(),
+            z: waterDispenser.IsoObject.getZ(),
+        }
+        sendClientCommand(character, 'WaterDispenser', 'TakeBottle', args)
 
         ISTimedActionQueue.getTimedActionQueue(character).onCompleted(action)
         ISLogSystem.logAction(action)
