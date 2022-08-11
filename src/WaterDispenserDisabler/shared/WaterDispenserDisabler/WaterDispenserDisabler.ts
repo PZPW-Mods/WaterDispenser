bf6deba46e492ac0f7ea5221613212b7258a0499
Konijima <mathieugermain1991@gmail.com>
2022-08-11 17:16:12 -0400

    Create WaterDispenserDisabler.ts

---
+++ src/WaterDispenserDisabler/shared/WaterDispenserDisabler/WaterDispenserDisabler.ts
@@ -0,0 +1,21 @@
+import { getActivatedMods, isClient, IsoObject, isServer } from "PipeWrench"
+import { loadGridSquare } from "PipeWrench-Events"
+
+if (getActivatedMods().contains("WaterDispenser") === false) {
+
+    loadGridSquare.addListener((square) => {
+        const objects = square.getObjects()
+        for (let i = 0; i < objects.size(); i++) {
+            const object = objects.get(i) as IsoObject
+            const modData = object.getModData()
+            if (modData.waterDispenserInfo && modData.waterDispenserInfo.type !== "Vanilla") {
+                object.setSpriteFromName("location_business_office_generic_01_49")
+                if (isServer()) object.transmitCompleteItemToClients()
+                if (isClient()) object.transmitCompleteItemToServer()
+                modData.waterDispenserInfo = null
+                object.transmitModData()
+            }
+        }
+    })
+
+}
